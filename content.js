d799325d906835a998395528146c37f6a1de1329
rk143183love <kirtanegaurav06@gmail.com>
2026-09-17 10:48:46 +0530

    Add editable website content model

---
+++ content.js
@@ -0,0 +1,6 @@
+window.ATM_CONTENT = {
+  services: [
+    'Labour Contracting Services','Loading & Unloading / Material Handling','Housekeeping Services','Job Work & Electrical Maintenance','Skilled / Unskilled / Technical Manpower','Gardening Services','Security Services','Placement & Staffing Services','Catering Services','Guest House Management','Supply Chain Management','Pest Control Services','Transport Service','Scrap Material Supply','Computer Accessories'
+  ],
+  clients: ['PINNACLE POLYMERS PVT LTD.','DHANANJAY LLP','GLOBAL INDIA PVT. LTD.','AIRPOINT PVT. LTD.','TAC LOGISTICS','SAFEXPRESS']
+};
